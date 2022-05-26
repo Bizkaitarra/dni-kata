f28c9a17a679ab72a39645c8aafb68f401627c9a
Jon González Garrido <bizkaitarra@gmail.com>
2022-05-26 13:06:30 +0200

    In green because we throw exception when first 8 chars are not numeric

--- src/Dni.php
+++ src/Dni.php
@@ -10,7 +10,10 @@ class Dni
     public function __construct(private string $value)
     {
         if (strlen($this->value) != self::ALLOWED_LENGTH) {
-            throw new LengthException();
+            throw new \LengthException();
+        }
+        if (!is_numeric(substr($this->value, 0, 8))) {
+            throw new \DomainException('First 8 characters should be numeric');
         }
     }
 
