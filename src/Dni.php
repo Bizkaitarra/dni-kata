bbefabaf6f84637713ab67e62af259613e0ebdba
Jon González Garrido <bizkaitarra@gmail.com>
2022-05-26 13:08:41 +0200

    Refactor

--- src/Dni.php
+++ src/Dni.php
@@ -9,9 +9,25 @@ class Dni
 
     public function __construct(private string $value)
     {
+        $this->assertLengthIsOk();
+        $this->assertFirstEightCharsAreNumeric();
+    }
+
+    /**
+     * @return void
+     */
+    public function assertLengthIsOk(): void
+    {
         if (strlen($this->value) != self::ALLOWED_LENGTH) {
             throw new \LengthException();
         }
+    }
+
+    /**
+     * @return void
+     */
+    public function assertFirstEightCharsAreNumeric(): void
+    {
         if (!is_numeric(substr($this->value, 0, 8))) {
             throw new \DomainException('First 8 characters should be numeric');
         }
